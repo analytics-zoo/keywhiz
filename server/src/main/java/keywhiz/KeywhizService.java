a773ff63a4efc2f4e9a7ae2ac4725c5313f34dd9
Heyang Sun <60865256+Uxito-Ada@users.noreply.github.com>
2023-04-04 10:29:09 +0800

    Revert "fix"

--- server/src/main/java/keywhiz/KeywhizService.java
+++ server/src/main/java/keywhiz/KeywhizService.java
@@ -66,8 +66,7 @@ import keywhiz.service.resources.automation.v2.ClientResource;
 import keywhiz.service.resources.automation.v2.GroupResource;
 import keywhiz.service.resources.automation.v2.SecretResource;
 import org.flywaydb.core.Flyway;
-import io.dropwizard.logging.LoggingFactory;
-import io.dropwizard.logging.ExternalLoggingFactory;
+import io.dropwizard.logging.{LoggingFactory, ExternalLoggingFactory}
 
 /**
  * Starting point for Keywhiz, an implementation of the Dropwizard Service.
